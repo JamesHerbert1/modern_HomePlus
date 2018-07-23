522ea1e945014ce3dd47153ebdb91fea408ae0d7
kamransumar <kamransumar@gmail.com>
2018-07-23 16:47:41 +0300

    added background

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function w3_open() {
-  document.getElementById("mySidebar").style.width = "100%";
-  document.getElementById("mySidebar").style.display = "block";
+ document.getElementById("mySidebar").style.width = "100%";
+ document.getElementById("mySidebar").style.display = "block";
 }
 
 function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+ document.getElementById("mySidebar").style.display = "none";
 }
 
 // HAPPY CUSTOMERS SCRIPT
@@ -13,31 +13,31 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+ showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+ showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+ var i;
+ var slides = document.getElementsByClassName("mySlides");
+ var dots = document.getElementsByClassName("dot");
+ if (n > slides.length) {
+   slideIndex = 1
+ }
+ if (n < 1) {
+   slideIndex = slides.length
+ }
+ for (i = 0; i < slides.length; i++) {
+   slides[i].style.display = "none";
+ }
+ for (i = 0; i < dots.length; i++) {
+   dots[i].className = dots[i].className.replace(" active", "");
+ }
+ slides[slideIndex - 1].style.display = "block";
+ dots[slideIndex - 1].className += " active";
 }
 
 // DROPDOWN MENU
@@ -45,20 +45,45 @@ function showSlides(n) {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+ document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+ if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+   var dropdowns = document.getElementsByClassName("dropdown-content");
+   var i;
+   for (i = 0; i < dropdowns.length; i++) {
+     var openDropdown = dropdowns[i];
+     if (openDropdown.classList.contains('show')) {
+       openDropdown.classList.remove('show');
+     }
+   }
+ }
 }
+
+
+
+
+// BOOKING form
+
+(function($) {
+ function floatLabel(inputType) {
+   $(inputType).each(function() {
+     var $this = $(this);
+     // on focus add cladd active to label
+     $this.focus(function() {
+       $this.next().addClass("active");
+     });
+     //on blur check field and remove class if needed
+     $this.blur(function() {
+       if ($this.val() === '' || $this.val() === 'blank') {
+         $this.next().removeClass();
+       }
+     });
+   });
+ }
+ // just add a class of "floatLabel to the input field!"
+ floatLabel(".floatLabel");
+})(jQuery);
