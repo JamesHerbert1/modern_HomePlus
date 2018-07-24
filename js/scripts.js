472470f27dad1a0feb31669a08e782709c9c5c76
JamesHerbert1 <agembejames1997@gmail.com>
2018-07-24 22:07:53 +0300

    added my top naviagtion bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,31 @@
-function w3_open() {
- document.getElementById("mySidebar").style.width = "100%";
- document.getElementById("mySidebar").style.display = "block";
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
 }
 
-function w3_close() {
- document.getElementById("mySidebar").style.display = "none";
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 // HAPPY CUSTOMERS SCRIPT
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
- showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
- showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;
+  if (slideIndex > slides.legnth) {slideIndex = 1}
+  for (i = 0; i < dots.legnth; i++) {
+    dots[i].className = dots[i].className.replace("active","");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000);
 }
 
 // DROPDOWN MENU
